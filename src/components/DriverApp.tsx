1245a7daa85a403e53ef7c58a9e44e64e650135b
Tomoko Takami <t2tato01@students.oamk.fi>
2023-11-17 16:32:06 +0200

    fetching data part are added

--- src/components/DriverApp.tsx
+++ src/components/DriverApp.tsx
@@ -2,23 +2,64 @@
 import React, { useState } from 'react';
 import ActiveParcelLockerSelector from './ActiveParcelLockerSelector';
 import FreeCabinetsList from './FreeCabinetsList';
+import Pickup from './Pickup'; // Pickup コンポーネントの追加
+import Deliver from './Deliver'; // Deliver コンポーネントの追加
 
 import '../App.css';
 
 const DriverApp: React.FC = () => {
   const [selectedLocker, setSelectedLocker] = useState<string | null>(null);
+  const [viewMode, setViewMode] = useState<string>('freeCabinets'); // 表示モードの状態を管理
 
+  // ロッカーが選択されたときのハンドラー
   const handleLockerSelect = (lockerId: string) => {
     setSelectedLocker(lockerId);
+  };
+
+  // 表示モードを切り替えるハンドラー
+  const handleViewModeChange = (mode: string) => {
+    setViewMode(mode);
   };
 
   return (
     <>
       <h1>Driver App</h1>
+      {/* アクティブなパーセルロッカーの選択コンポーネント */}
       <ActiveParcelLockerSelector onSelect={handleLockerSelect} />
-      {selectedLocker && <p>Selected Parcel Locker: {selectedLocker}</p>}
-      <FreeCabinetsList lockerId={selectedLocker || ''} />
-      {/* Add other components and features for parcel pick up, and parcel delivery */}
+
+      {/* ビューモードを切り替えるためのボタンなど */}
+      <div>
+        <button onClick={() => handleViewModeChange('freeCabinets')}>Free Cabinets</button>
+        <button onClick={() => handleViewModeChange('pickup')}>Pickup</button>
+        <button onClick={() => handleViewModeChange('deliver')}>Deliver</button>
+      </div>
+
+      {/* 表示モードに基づいてコンポーネントを切り替え */}
+      {viewMode === 'freeCabinets' && (
+        <>
+          {selectedLocker && <p>Selected Parcel Locker: {selectedLocker}</p>}
+          {/* 空きキャビネットのリストを表示するコンポーネント */}
+          <FreeCabinetsList lockerId={selectedLocker || ''} />
+        </>
+      )}
+
+      {viewMode === 'pickup' && (
+        <>
+          {selectedLocker && <p>Selected Parcel Locker: {selectedLocker}</p>}
+          {/* Pickup コンポーネント */}
+          <Pickup lockerId={selectedLocker || ''} />
+        </>
+      )}
+
+      {viewMode === 'deliver' && (
+        <>
+          {selectedLocker && <p>Selected Parcel Locker: {selectedLocker}</p>}
+          {/* Deliver コンポーネント */}
+          <Deliver lockerId={selectedLocker || ''} />
+        </>
+      )}
+
+      
     </>
   );
 };
--- src/components/Pickup.tsx
+++ src/components/Pickup.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+interface PickupProps {
+  lockerId: string;
+}
+
 interface Cabinet {
   id: number;
   locker_id: number;
@@ -9,47 +13,34 @@ interface Cabinet {
   parcel: any; // Replace 'any' with the actual type of 'parcel'
 }
 
-interface FreeCabinetsListProps {
-  lockerId: string;
-}
-
-const FreeCabinetsList: React.FC<FreeCabinetsListProps> = ({ lockerId }) => {
+const Pickup: React.FC<PickupProps> = ({ lockerId }) => {
   const [cabinetStates, setCabinetStates] = useState<string[]>([]);
-  const [cabinets, setCabinets] = useState<Cabinet[]>([]); // Add this line to declare 'cabinets'
+  const [cabinets, setCabinets] = useState<Cabinet[]>([]);
 
-  // Fetch cabinet states from the backend
   const fetchCabinetStates = async () => {
     try {
       const response = await axios.get(`http://localhost:3000/api/lockers/${lockerId}`);
-      
-      // Assuming response.data.cabinets is an array of cabinets
       const fetchedCabinets: Cabinet[] = response.data.cabinets || [];
-      
-      // Extract cabinet_status from the cabinets
       const cabinetStatusArray = fetchedCabinets.map((cabinet) => cabinet.cabinet_status);
-
       console.log('cabinetStatusArray:', cabinetStatusArray);
       setCabinetStates(cabinetStatusArray);
-      setCabinets(fetchedCabinets); // Set 'cabinets' state
+      setCabinets(fetchedCabinets);
     } catch (error) {
       console.error('Error fetching cabinet states:', error);
     }
   };
 
   useEffect(() => {
-    // Fetch cabinet states from the backend
     fetchCabinetStates();
   }, [lockerId]);
 
-  // Function to arrange cabinets in the specified format
   const arrangeCabinets = () => {
     const arrangedCabinets: JSX.Element[] = [];
 
     for (let col = 0; col < 4; col++) {
       for (let row = 0; row < 4; row++) {
         const cabinetNumber = row * 4 + col + 1;
-
-        const isFree = cabinets[cabinetNumber - 1]?.parcel == null;
+        const isPickup = cabinetStates[cabinetNumber - 1] === 'in-use' && cabinets[cabinetNumber - 1]?.parcel !== null;
 
         arrangedCabinets.push(
           <div
@@ -59,7 +50,7 @@ const FreeCabinetsList: React.FC<FreeCabinetsListProps> = ({ lockerId }) => {
               height: '80px',
               border: '1px solid black',
               textAlign: 'center',
-              backgroundColor: isFree ? 'green' : 'white', // Adjust color for DELIVER
+              backgroundColor: isPickup ? 'blue' : 'white', // Adjust color for PICKUP
               color: 'black',
               display: 'flex',
               flexDirection: 'column',
@@ -76,12 +67,9 @@ const FreeCabinetsList: React.FC<FreeCabinetsListProps> = ({ lockerId }) => {
     return arrangedCabinets;
   };
 
-  // Add console.log for cabinetStates
-  console.log('cabinetStates:', cabinetStates);
-
   return (
     <div>
-      <h2>Free Cabinets (nothing inside) at Parcel Locker {lockerId}</h2>
+      <h2>Pickup Cabinets at Parcel Locker {lockerId}</h2>
       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 80px)', gap: '10px' }}>
         {arrangeCabinets()}
       </div>
@@ -89,4 +77,4 @@ const FreeCabinetsList: React.FC<FreeCabinetsListProps> = ({ lockerId }) => {
   );
 };
 
-export default FreeCabinetsList;
+export default Pickup;
